5339366aa2bb07730f41fc5184b5f09ca9b00802
safaacht <safaachtaoui@gmail.com>
2026-02-04 16:02:42 +0100

    login controller pour creation et sauvgarde

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\View;
+
+class LoginController
+{
+    public function create(){
+        return view('auth.login');
+    }
+    public function store(Request $request){
+        $check= Auth::attempt([
+        
+            'email'=>$request->email,
+            'password'=>$request->password
+        ]);
+
+        if(Auth::attempt()){
+            $request->session()->regenerate();
+
+            if($check){
+                if(Auth::user()->role=="admin"){
+                    return redirect('admin.dashboard');
+                }else{
+                    return redirect()->route('client');
+                }
+            }
+            
+        }else{
+            return redirect()->back()->with("error","Email or password is incorrect");
+
+        }
+    }
+}
