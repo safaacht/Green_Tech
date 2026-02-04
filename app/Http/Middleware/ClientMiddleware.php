b973f20257fcdb7641b9759188536241e0d7f99a
safaacht <safaachtaoui@gmail.com>
2026-02-04 16:01:38 +0100

    client middleware

---
+++ app/Http/Middleware/ClientMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Container\Attributes\Auth as AttributesAuth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response as FacadesResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class ClientMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(Auth::check() && Auth::user()->role==='client'){
+            return $next($request);
+        }    
+
+        abort(403, 'Forbidden');
+    }
+}
