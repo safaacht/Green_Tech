8794c44e9f9215a1632217b49fbcbae297d75966
safaacht <safaachtaoui@gmail.com>
2026-02-04 11:09:20 +0100

    modif du route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('product', ProductController::class);
-// Route::get('/product/{id}', [ProductController::class, 'show']);
-// Route::get('/', ProductController::class .'@create');
+Route::resource('products', ProductController::class);
