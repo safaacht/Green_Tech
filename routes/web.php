f59db05edbddbb8dbea1be9b033b357a5edcdf7e
safaacht <safaachtaoui@gmail.com>
2026-02-06 01:12:47 +0100

    l'ajout des routes de login,register,admin et client

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,29 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+include 'admin.php';
+include 'client.php';
 Route::resource('products', ProductController::class);
+
+
+Route::controller(RegisterController::class)->group(function(){
+    Route::get('/register','create')->name('register.create');
+    Route::post('/register','store')->name('register.store');
+
+});
+
+Route::controller(LoginController::class)->group(function(){
+    Route::get('/login','create')->name('login.create');
+    Route::post('/login','store')->name('login.store');
+    Route::post('/logout','destroy')->name('destroy');
+
+});
