eb4e6c2b860765576425c50d6fd1a2672d62e483
safaacht <safaachtaoui@gmail.com>
2026-02-06 01:03:58 +0100

    fix du path de use

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
