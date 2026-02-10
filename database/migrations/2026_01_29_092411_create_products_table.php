828df0cf810b9717c4ec1b7414ca3428f75fb59f
safaacht <safaachtaoui@gmail.com>
2026-02-10 16:21:52 +0100

    modif des types

--- database/migrations/2026_01_29_092411_create_products_table.php
+++ database/migrations/2026_01_29_092411_create_products_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('description');
+            $table->text('description');
             $table->decimal('price', 8, 2);
-            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
--- database/migrations/2026_02_04_120129_favoris.php
+++ database/migrations/2026_02_04_120129_favoris.php
@@ -13,11 +13,10 @@ return new class extends Migration
     {
         Schema::create('favoris',function(Blueprint $table){
             $table->id();
-            $table->foreignId('product_id')->constrained('products');
-            $table->foreignId('user_id')->constrained('users');
-
-
-        });
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->unique(['user_id', 'product_id']);
+            $table->timestamps();        });
     }
 
     /**
