2b621c0ca7b38ea3b442be8d5a0b395b6b39b7b1
safaacht <safaachtaoui@gmail.com>
2026-02-10 16:27:26 +0100

    modif de route en ajoutant userController et roleController

--- routes/admin.php
+++ routes/admin.php
@@ -2,8 +2,12 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RolesController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth','admin'])->group(function(){
     Route::get('/dashboard',[AdminController::class,'dashboard'])->name('admin.dashboard');
+    Route::resource('users', UserController::class);
+    Route::resource('roles', RolesController::class);
 });
