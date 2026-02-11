7db8e1dc6d1b47109c48d10da39349a7c339d187
safaacht <safaachtaoui@gmail.com>
2026-02-11 20:15:12 +0100

    ajout du route d'export les infos en excel

--- routes/admin.php
+++ routes/admin.php
@@ -5,9 +5,15 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\RolesController;
 use Illuminate\Support\Facades\Route;
+use App\Exports\UsersExport;
+use Maatwebsite\Excel\Facades\Excel;
 
 Route::middleware(['auth','admin'])->group(function(){
     Route::get('/dashboard',[AdminController::class,'dashboard'])->name('admin.dashboard');
     Route::resource('users', UserController::class);
     Route::resource('roles', RolesController::class);
+    Route::get('/export-users',function(){
+        return Excel::download(new UsersExport, 'users.xlsx');
+        })->name('admin.export');
+    
 });
